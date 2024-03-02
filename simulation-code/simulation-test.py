c28ad7961667ebf738bcbbd4f73be00b7d4b0964
Samarjit Kaushik <samarjit@Samarjits-MacBook-Air.local>
2024-03-01 21:34:30 -0800

    updated readme, created simulation prototype

--- simulation-code/simulation-test.py
+++ simulation-code/simulation-test.py
@@ -10,13 +10,14 @@ def main():
     ]
     n = 10 ** 5
     init_config = {a: 0.51 * n, b: 0.49 * n}
+    print(init_config)
     sim = Simulation(init_config, approx_majority)
     sim.run()
     sim.history.plot()
     plt.title('approximate majority protocol')
     plt.xlim(0, sim.times[-1])
     plt.ylim(0, n)
-    plt.show()
+    plt.show
 
 if __name__ == '__main__':
     main()
